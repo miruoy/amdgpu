b9697a0f3a854da93c7dca1e0849af031af69baf
miruoy <MIRUOY\miruoy@blue.miruoy.doesntexist.com>
2018-06-30 14:11:55 +0200

    modified:   amdgpu-setclock.sh

--- amdgpu-setclock.sh
+++ amdgpu-setclock.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 cat /sys/class/drm/card0/device/pp_dpm_sclk
+cat /sys/class/drm/card0/device/power_dpm_force_performance_level
 
 echo manual > /sys/class/drm/card0/device/power_dpm_force_performance_level
-read -e -p "Please input new clockspeed: " -i "0" SETCLOCK
+read -e -p "Please input new clockspeed: " -i "" SETCLOCK
 echo $SETCLOCK > /sys/class/drm/card0/device/pp_dpm_sclk
+sleep 1
 cat /sys/class/drm/card0/device/pp_dpm_sclk
 
 exit
