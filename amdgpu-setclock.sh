86febc0b203dd789a16d85ef7992392a353bec0b
miruoy <yourimatthys@gmail.com>
2018-06-30 14:15:00 +0200

    Update amdgpu-setclock.sh

--- amdgpu-setclock.sh
+++ amdgpu-setclock.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+printf "Available and currently selected clock speed:\n"
 cat /sys/class/drm/card0/device/pp_dpm_sclk
+prinft "Current Power Profile:\n"
 cat /sys/class/drm/card0/device/power_dpm_force_performance_level
-
+printf "Setting Profile to Manual\n"
 echo manual > /sys/class/drm/card0/device/power_dpm_force_performance_level
 read -e -p "Please input new clockspeed: " -i "" SETCLOCK
 echo $SETCLOCK > /sys/class/drm/card0/device/pp_dpm_sclk
 sleep 1
+printf "Available and currently selected clock speed:\n"
 cat /sys/class/drm/card0/device/pp_dpm_sclk
 
 exit
